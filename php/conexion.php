2a9429642bd45a7dbef290fc06cd8298133846dd
kikecaba09 <139581484+kikecaba09@users.noreply.github.com>
2024-05-28 13:34:15 -0500

    Avance-Licorería

---
+++ php/conexion.php
@@ -0,0 +1,13 @@
+<?php
+
+$nombredelservidor = "localhost:8080";
+$nombredeusuario = "root";
+$contrasena = "";
+$basededatos = "licoreriayolo";
+
+$conn = new mysqli($nombredelservidor, $nombredeusuario, $contrasena, $basededatos);
+
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+?>
