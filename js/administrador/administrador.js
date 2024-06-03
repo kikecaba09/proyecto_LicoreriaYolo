1b13185a224ad88c718ff3a7ba07f651422eccbe
kikecaba09 <139581484+kikecaba09@users.noreply.github.com>
2024-06-03 09:19:36 -0500

    avance

---
+++ js/administrador/administrador.js
@@ -0,0 +1,10 @@
+function cargarContenido(seccion) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("contenido-dinamico").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", seccion + ".html", true);
+    xhttp.send();
+}
