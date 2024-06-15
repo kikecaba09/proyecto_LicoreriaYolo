723e6740b6a449bad2d70bcfe4586876e96d2069
kikecaba09 <139581484+kikecaba09@users.noreply.github.com>
2024-06-14 20:07:17 -0500

    avance

--- php/pedido.php
+++ php/pedido.php
@@ -11,7 +11,16 @@ if(isset($_POST['registroPedido'])){
         strlen($_POST['metodoPago'])>=1 &&
         strlen($_POST['comentario'])>=1 
     ){
+        $nombreCompleto = trim($_POST['nombreCompleto']);
+        $telefono = trim($_POST['telefono']);
+        $direccion = trim($_POST['direccion']);
+        $producto = trim($_POST['producto']);
+        $cantidad = trim($_POST['cantidad']);
+        $metodoPago = trim($_POST['metodoPago']);
+        $comentario = trim($_POST['comentario']);
+        $fecha = date("d/m/y");
 
+        
     }
     
 }
