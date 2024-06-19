2f1c563bcf133999a375eb564a0aacf02f9609ff
kikecaba09 <139581484+kikecaba09@users.noreply.github.com>
2024-06-19 04:53:27 -0500

    avance

--- src/js/login/loginCliente.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('.titulo-principal').addClass('animate__fadeInDown');
-    $('.cliente-login-container').addClass('animate__bounceInUp');
-    $('form').on('submit', function(event) {
-        // Añade una animación de salida al enviar el formulario
-        $('.cliente-login-container').addClass('animate__fadeOut');
-    });
-});
